264cbb2eb4f53d5cefda4e2263909f86b249c178
Idir6380 <87910077+Idir6380@users.noreply.github.com>
2023-05-03 01:24:19 +0100

    Add files via upload

--- dataPreparation.py
+++ dataPreparation.py
@@ -12,7 +12,7 @@ os.makedirs(openEyesDir, exist_ok=True)
 rawDataDir = 'mrlEyes_2018_01'
 for dirPath, dirName, fileNames in os.walk(rawDataDir):
     for i in [f for f in fileNames if f.endswith('.png')]:
-        if i.split('_')[4] == 0:
+        if i.split('_')[4] == '0':
             shutil.copy(src=dirPath + '/' + i, dst=closedEyesDir)
         elif i.split('_')[4] == '1':
             shutil.copy(src=dirPath + '/' + i, dst=openEyesDir)
